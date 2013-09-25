f60bf7663192a4004ccd8609b29395cbffadb970
kushnarev <avkushnarev@gmailo.com>
2013-09-25 17:21:52 +0400

    Massive refactor. Build class for handle feeds.

--- fetcher.py
+++ fetcher.py
@@ -1,51 +1,74 @@
+#!/usr/bin/python
+
 import feedparser
+import logging
 from time import mktime
 from datetime import datetime
-from feeds import models, connection
+from feeds import connection
 
-URLS = ['http://feeds.feedburner.com/d0od',
-        'http://habrahabr.ru/rss/hubs/',
-        'http://dribbble.com/shots/popular.rss',
-        'http://www.alexconrad.org//feeds/posts/default',
-        'http://www.ixbt.com/export/articles.rss',
-        'http://feeds.feedburner.com/eaxme',
-        'http://feeds.feedburner.com/futurecolors'
-        ]
 
-def add_new_feed(url):
-    new_feed = feedparser.parse(url)
-    items = []
-    for article in new_feed.entries:
-        items.append({'summary': article.summary,
-                      'title': article.title,
-                      'link': article.link,
-                      'published': datetime.fromtimestamp(mktime(article.published_parsed)),
-                      'viewed': False})
+class FeedHandler(object):
+    """This class response for adding and updating feeds"""
 
-    feed = connection.test.feed.Feed()
-    if  not (new_feed.get('feed').get('title') and new_feed.get('url')):
-        print new_feed.get('feed')
-        raise ValueError, "Invalide feed format"
+    def __init__(self, url):
+        parsed_feed = feedparser.parse(url)
+        self.url = unicode(url)
+        self.title = unicode(parsed_feed.feed.title)
+        self.items = self.__extract_items(parsed_feed)
+        logging.info("Processed %s(%s)", self.title, url)
 
-    feed['title'] = unicode(new_feed.feed.title)
-    feed['url'] = unicode(url)
-    feed['items'] = items
-    feed.save()
+    @staticmethod
+    def __extract_items(parsed_feed):
+        """Extract items from parsed feed object"""
+        return [{'summary': article.summary,
+                 'title': article.title,
+                 'link': article.link,
+                 'published': datetime.fromtimestamp(mktime(article.published_parsed)),
+                 'viewed': False}
+                for article in parsed_feed.entries]
 
-def find_feed(url):
-    return connection.test.feed.find_one({'url': url})
+    @property
+    def in_db(self):
+        """Return True if Feed already in DB"""
+        return connection.test.feed.find_one({'url': self.url})
 
-def update_feed(url):
-    updated_feed = feedparser.parse(url)
-    items = []
-    for article in updated_feed.entries:
-        items.append(article.summary)
-    connection.test.feed.find_and_modify({'url': url}, {'$push': {'items': {'$each': items}}})
+    def __add_feed_in_db(self):
+        """Use if feed not stored in db"""
+        logging.info("Create new feed (%s(%s)) in db", self.title, self.url)
+        collection = connection.test.feed.Feed()
+        if not self.title and self.url:
+            logging.warning("Issue with feed parsing: %s", self.url)
+            raise ValueError("Invalid %s feed format" % self.url)
+
+        collection['title'] = self.title
+        collection['url'] = self.url
+        collection['items'] = self.items
+        collection.save()
+
+    def __update_feed_in_db(self):
+        """Use if feed already in db"""
+        logging.info("Update %s(%s)", self.title, self.url)
+        connection.test.feed.find_and_modify({'url': self.url}, {'$push': {'items': {'$each': self.items}}})
+
+    def process(self):
+        """Common method for create od update feed in db"""
+        if self.in_db:
+            self.__update_feed_in_db()
+        else:
+            self.__add_feed_in_db()
+
 
 if __name__ == "__main__":
-
+    
+    URLS = ['http://feeds.feedburner.com/d0od',
+            'http://habrahabr.ru/rss/hubs/',
+            'http://dribbble.com/shots/popular.rss',
+            'http://www.alexconrad.org//feeds/posts/default',
+            'http://www.ixbt.com/export/articles.rss',
+            'http://feeds.feedburner.com/eaxme',
+            'http://feeds.feedburner.com/futurecolors'
+            ]
+    
     for url in URLS:
-        if find_feed(url):
-            update_feed(url)
-        else:
-            add_new_feed(url)
+        f = FeedHandler(url)
+        f.process()
